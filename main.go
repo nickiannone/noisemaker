c12aab05ea729a144372e112cedf30f31c086d69
Nick Iannone <nickiannone@gmail.com>
2024-11-05 12:59:56 -0600

    Tested a few scenarios, cleaned up code, fixed various bugs, still need tests!

--- main.go
+++ main.go
@@ -11,15 +11,15 @@ import (
 	"net/http/httptrace"
 	"net/url"
 	"os"
+	"os/exec"
 	"os/user"
-	"reflect"
 	"runtime"
 	"strconv"
 	"strings"
 	"time"
 )
 
-const HeaderStr = "timestamp,activity,os,username,processName,processCmd,pid,path,status,sourceAddr,sourcePort,destAddr,destPort,bytesSent,protocol,responseStatusCd,responseBody"
+const HeaderStr = "timestamp,activity,os,username,processName,processCmd,pid,path,status,method,sourceAddr,sourcePort,destAddr,destPort,bytesSent,protocol"
 
 type ActivityLogEntry struct {
 	timestamp   		string  `csv:"timestamp"`   		// RFC3339 timestamp
@@ -40,8 +40,99 @@ type ActivityLogEntry struct {
 	destPort   			int     `csv:"destPort"`   			// destination port
 	bytesSent  			int     `csv:"bytesSent"`  			// number of bytes transmitted
 	protocol   			string  `csv:"protocol"`   			// the protocol used (http:, ftp:, udp:, etc.)
-	responseStatusCd 	int     `csv:"responseStatusCd"`	// the response status code from the request
-	responseBody		string	`csv:"responseBody"`		// the response body (with newlines and commas escaped)
+	// responseStatusCd 	int     `csv:"responseStatusCd"`	// the response status code from the request
+	// responseBody		string	`csv:"responseBody"`		// the response body (with newlines and commas escaped)
+}
+
+// TODO: Replace this with something that uses the field annotations above!
+func serializeToCSV(logInfo *ActivityLogEntry) []string {
+	return []string{
+		logInfo.timestamp,
+		logInfo.activity,
+		logInfo.os,
+		logInfo.username,
+		logInfo.processName,
+		logInfo.processCmd,
+		strconv.Itoa(logInfo.processId),
+		logInfo.path,
+		logInfo.status,
+		logInfo.method,
+		logInfo.sourceAddr,
+		strconv.Itoa(logInfo.sourcePort),
+		logInfo.destAddr,
+		strconv.Itoa(logInfo.destPort),
+		strconv.Itoa(logInfo.bytesSent),
+		logInfo.protocol,
+		// strconv.Itoa(logInfo.responseStatusCd),
+		// logInfo.responseBody,
+	}
+}
+
+// TODO: Refactor this to use some sort of mapping!
+func deserializeFromCSV(row []string) (*ActivityLogEntry, error) {
+	if len(row) < 18 {
+		check(fmt.Errorf("not enough fields in row %v to load activity log entry! (18 required, %d found)", row, len(row)))
+	}
+
+	pidVal, err := strconv.Atoi(row[6])
+	if err != nil {
+		pidVal = 0
+	}
+	sourcePortVal, err := strconv.Atoi(row[11])
+	if err != nil {
+		sourcePortVal = 0
+	}
+	destPortVal, err := strconv.Atoi(row[13])
+	if err != nil {
+		destPortVal = 0
+	}
+	bytesSentVal, err := strconv.Atoi(row[14])
+	if err != nil {
+		bytesSentVal = 0
+	}
+	// responseStatusCdVal, err := strconv.Atoi(row[16])
+	// if err != nil {
+	// 	responseStatusCdVal = -1
+	// }
+
+	logInfo := new(ActivityLogEntry)
+	logInfo.timestamp = row[0]
+	logInfo.activity = row[1]
+	logInfo.os = row[2]
+	logInfo.username = row[3]
+	logInfo.processName = row[4]
+	logInfo.processCmd = row[5]
+	logInfo.processId = pidVal
+	logInfo.path = row[7]
+	logInfo.status = row[8]
+	logInfo.method = row[9]
+	logInfo.sourceAddr = row[10]
+	logInfo.sourcePort = sourcePortVal
+	logInfo.destAddr = row[12]
+	logInfo.destPort = destPortVal
+	logInfo.bytesSent = bytesSentVal
+	logInfo.protocol = row[15]
+	// logInfo.responseStatusCd = responseStatusCdVal
+	// logInfo.responseBody = row[17]
+
+	return logInfo, nil
+}
+
+func splitCSVRow(rowText string) ([]string, error) {
+	reader := csv.NewReader(strings.NewReader(rowText))
+	fields, err := reader.Read()
+	if err != nil && err != io.EOF {
+		return nil, err
+	}
+	return fields, nil
+}
+
+func fileExists(path string) bool {
+	info, err := os.Stat(path)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return !info.IsDir()
 }
 
 // Usage: noisemaker [opts...] <command> [args...]
@@ -59,70 +150,116 @@ func main() {
 	// Determine which OS we're on ('darwin', 'linux', etc.)
 	currentOS := runtime.GOOS
 
-	// Determines the current user
-	currentUser, err := user.Current()
-	check(err)
-
-	// Parse CLI flags
-	logFilePathPtr := flag.String("logfile", "./activity-log.csv", "the path to the activity log CSV file")
-	overwritePtr := flag.Bool("overwrite", false, "whether to overwrite (true) or append to (false) the activity log CSV file (default false)")
-
-	// Parse flags
-	flag.Parse()
-	logFilePath := *logFilePathPtr
-	overwrite := *overwritePtr
-	logFileMode := os.O_APPEND | os.O_CREATE | os.O_WRONLY
-	if overwrite {
-		logFileMode = os.O_RDWR | os.O_CREATE
-	}
-
-	// Get the command and args
-	remainingArgs := flag.Args()
-	command := remainingArgs[0]
-	commandArgs := remainingArgs[1:]
-
-	// Check the activity log to see if it already has a header; if not, write one.
-	writeHeader := true
-	if !overwrite {
-		peekActivityLogFile, err := os.Open(logFilePath)
-		check(err)
-		defer peekActivityLogFile.Close()
-
-		scanner := bufio.NewScanner(peekActivityLogFile)
-		if scanner.Scan() {
-			firstLine := scanner.Text()
-			if firstLine == "" {
-				fmt.Println("Empty first line, assuming no header")
-				writeHeader = true
-			} else if isHeader(firstLine) {
-				fmt.Println("Header detected on first line")
-				writeHeader = false
-			} else {
-				fmt.Println("Header not detected on first line, adding header line at current end of file")
-				// TODO: Shuffle the file's existing contents downwards and add a header?
-				writeHeader = true
-			}
-		} else if scanner.Err() != nil {
-			fmt.Println("Unable to open existing file for appending, it does not exist!")
-			writeHeader = true
-		}
-	}
-
-	// Open the activity log for writing
-	activityLogFile, err := os.OpenFile(logFilePath, logFileMode, 0644)
-	check(err)
-	defer activityLogFile.Close()
-
-	// If we're writing a header, do it now!
-	if writeHeader {
-		_, err := activityLogFile.WriteString(HeaderStr + "\n")
-		check(err)
-	}
-
 	// Get the current process name and PID
 	currentProcessId := os.Getpid()
 	currentProcessName, err := os.Executable()
 	check(err)
+
+	// Determines the current user
+	currentUser, err := user.Current()
+	check(err)
+
+	// Parse log file flags
+	logFilePathPtr := flag.String("logfile", "./activity-log.csv", "the path to the activity log CSV file")
+	overwritePtr := flag.Bool("overwrite", false, "whether to overwrite (true) or append to (false) the activity log CSV file (default false)")
+	flag.Parse()
+	logFilePath := *logFilePathPtr
+	overwrite := *overwritePtr
+
+	// Get the command and args
+	remainingArgs := flag.Args()
+	if len(remainingArgs) < 1 {
+		fmt.Printf("No command specified! Exiting...\n")
+		return
+	}
+	command := remainingArgs[0]
+	commandArgs := []string{}
+	if len(remainingArgs) > 1 {
+		commandArgs = remainingArgs[1:]
+	}
+
+	// Parse log entries from the existing log file, if any.
+	existingLogEntries := []*ActivityLogEntry{}
+	activityLogFileExists := fileExists(logFilePath)
+	peekActivityLogFile, err := os.OpenFile(logFilePath, os.O_RDONLY, 0644)
+	if activityLogFileExists && err != nil {
+		scanner := bufio.NewScanner(peekActivityLogFile)
+		if scanner.Scan() {
+			firstLine := scanner.Text()
+			fmt.Printf("First line: %s\n", firstLine)
+			if !isCSVHeaderStr(firstLine) {
+				// Try to parse it as a record, but fail gracefully
+				row, err := splitCSVRow(firstLine)
+				if err != nil {
+					fmt.Printf("Unable to tokenize first row, syntax error in '%s'!\n", firstLine)
+				}
+				parsedLogEntry, err := deserializeFromCSV(row)
+				if err != nil {
+					fmt.Printf("Unable to deserialize first row, parser error in %v\n", row)
+				}
+				if parsedLogEntry != nil {
+					fmt.Printf("Deserialized first row to %v\n", parsedLogEntry)
+					existingLogEntries = append(existingLogEntries, parsedLogEntry)
+				}
+			}
+
+			// Read the other rows
+			for scanner.Scan() {
+				existingRow := scanner.Text()
+				// Try to parse it as a record, and skip ahead if we fail anywhere
+				row, err := splitCSVRow(existingRow)
+				if err != nil {
+					fmt.Printf("Unable to tokenize row, syntax error in '%s'!\n", existingRow)
+					continue
+				}
+				parsedLogEntry, err := deserializeFromCSV(row)
+				if err != nil {
+					fmt.Printf("Unable to deserialize first row, parser error in %v\n", row)
+					continue
+				}
+				if parsedLogEntry != nil {
+					fmt.Printf("Deserialized first row to %v\n", parsedLogEntry)
+					existingLogEntries = append(existingLogEntries, parsedLogEntry)
+				}
+			}
+		} else if scanner.Err() != nil {
+			fmt.Println("Unable to open existing file for appending, it does not exist!")
+		}
+	}
+	peekActivityLogFile.Close()
+
+	// Open the activity log for writing
+	var activityLogFile *os.File
+	var writeHistoricalRecords bool
+	if activityLogFileExists && !overwrite {
+		fmt.Printf("Opening existing log file %s for appending...\n", logFilePath)
+		activityLogFile, err = os.OpenFile(logFilePath, os.O_APPEND | os.O_CREATE | os.O_WRONLY, 0644)
+		writeHistoricalRecords = false
+	} else if activityLogFileExists && overwrite {
+		fmt.Printf("Opening existing log file %s for overwriting...\n", logFilePath)
+		activityLogFile, err = os.OpenFile(logFilePath, os.O_RDWR | os.O_CREATE, 0644)
+		writeHistoricalRecords = true
+	} else {
+		fmt.Printf("Creating new log file %s...\n", logFilePath)
+		activityLogFile, err = os.Create(logFilePath)
+		writeHistoricalRecords = true
+	}
+	check(err)
+	defer activityLogFile.Close()
+
+	// Write the header and old records
+	if writeHistoricalRecords {
+		// Write header
+		_, err = activityLogFile.WriteString(HeaderStr + "\n")
+		check(err)
+
+		// Write all other existing log entries
+		for _, logEntry := range existingLogEntries {
+			logEntryCSV := strings.Join(serializeToCSV(logEntry), ",")
+			_, err = activityLogFile.WriteString(logEntryCSV + "\n")
+			check(err)
+		}
+	}
 
 	// Create the initial activity log entry
 	activityLogEntry := new(ActivityLogEntry)
@@ -142,14 +279,21 @@ func main() {
 		procArgs := commandArgs[1:]
 		activityLogEntry.processCmd = escapeCommandString(procCmd, procArgs)
 
+		fmt.Printf("Running command %s with args %v\n", procCmd, procArgs)
 		process, err := startProcess(procCmd, procArgs)
 		check(err)
 
 		activityLogEntry.processId = process.Pid
 	case "create":
 		// Call createFile and capture the output
+		if len(commandArgs) < 1 {
+			check(fmt.Errorf("not enough arguments for create! Args: %v", commandArgs))
+		}
 		path := commandArgs[0]
-		contents := commandArgs[1]
+		var contents string = ""
+		if len(commandArgs) > 1 {
+			contents = commandArgs[1]
+		}
 
 		status, err := createFile(path, contents)
 		if err != nil {
@@ -181,12 +325,27 @@ func main() {
 		}
 	case "send":
 		// Send a message to a given receiver
-		method := commandArgs[0]
-		destAddr := commandArgs[1]
-		destPort, err := strconv.Atoi(commandArgs[2])
-		check(err)
-		protocol := commandArgs[3]
-		data := commandArgs[4]
+		method := http.MethodGet
+		if len(commandArgs) > 0 {
+			method = commandArgs[0]
+		}
+		destAddr := "192.168.0.1"
+		if len(commandArgs) > 1 {
+			destAddr = commandArgs[1]
+		}
+		destPort := 80
+		if len(commandArgs) > 2 {
+			destPort, err = strconv.Atoi(commandArgs[2])
+			check(err)
+		}
+		protocol := "http"
+		if len(commandArgs) > 3 {
+			protocol = commandArgs[3]
+		}
+		data := ""
+		if len(commandArgs) > 4 {
+			data = commandArgs[4]
+		}
 
 		// Record the parsed identifying information
 		activityLogEntry.method = method
@@ -198,6 +357,7 @@ func main() {
 		fmt.Printf("Sending %d bytes of data to %s %s (port %d) using protocol %s...\n", len(data), method, destAddr, destPort, protocol)
 
 		// Send it!
+		// TODO: Add header encoding somehow!
 		messageResponse, err := sendMessage(method, destAddr, destPort, protocol, nil, data)
 		if err != nil {
 			// TODO: Add more specific error handling?
@@ -211,8 +371,8 @@ func main() {
 		activityLogEntry.sourceAddr = messageResponse.sourceAddr
 		activityLogEntry.sourcePort = messageResponse.sourcePort
 		activityLogEntry.bytesSent = messageResponse.bytesSent
-		activityLogEntry.responseStatusCd = messageResponse.responseStatusCode
-		activityLogEntry.responseBody = escapeRawText(messageResponse.responseBody)
+		// activityLogEntry.responseStatusCd = messageResponse.responseStatusCode
+		// activityLogEntry.responseBody = escapeRawText(messageResponse.responseBody)
 	case "help":
 		// Print the help text?
 		// TODO
@@ -220,7 +380,9 @@ func main() {
 		check(fmt.Errorf("invalid command specified: %s", command))
 	}
 
-	writeActivityLog(activityLogFile, activityLogEntry)
+	logEntryCSV := strings.Join(serializeToCSV(activityLogEntry), ",")
+	_, err = activityLogFile.WriteString(logEntryCSV + "\n")
+	check(err)
 }
 
 func escapeCommandString(cmd string, args []string) string {
@@ -246,7 +408,7 @@ func createFile(path string, contents string) (string, error) {
 		return "error", err
 	}
 
-	fmt.Printf("%d bytes written to new file %s", bytesWritten, path)
+	fmt.Printf("%d bytes written to new file %s\n", bytesWritten, path)
 	return "created", nil
 }
 
@@ -265,7 +427,7 @@ func updateFile(path string, contents string) (string, error) {
 		return "error", err
 	}
 
-	fmt.Printf("%d bytes written to updated file %s", bytesWritten, path)
+	fmt.Printf("%d bytes written to updated file %s\n", bytesWritten, path)
 	return "updated", nil
 }
 
@@ -276,7 +438,7 @@ func deleteFile(path string) (string, error) {
 		return "error", err
 	}
 
-	fmt.Printf("File %s deleted", path)
+	fmt.Printf("File %s deleted\n", path)
 	return "deleted", nil
 }
 
@@ -297,21 +459,17 @@ func makeErrorResponse(status string, path string) *MessageResponse {
 	response.bytesSent = 0
 	response.status = status
 	response.path = path
-	response.responseBody = ""
-	response.responseStatusCode = -1
 
 	return response
 }
 
-func makeSuccessResponse(status string, responseBody string, sourceAddr string, sourcePort int, bytesSent int, path string, responseStatusCode int) *MessageResponse {
+func makeSuccessResponse(status string, sourceAddr string, sourcePort int, bytesSent int, path string) *MessageResponse {
 	response := new(MessageResponse)
 	response.sourceAddr = sourceAddr
 	response.sourcePort = sourcePort
 	response.bytesSent = bytesSent
 	response.status = status
 	response.path = path
-	response.responseBody = responseBody
-	response.responseStatusCode = responseStatusCode
 
 	return response
 }
@@ -351,16 +509,28 @@ func sendHttpMessage(method string, path string, headers any, body string) (*Mes
 	trace := &httptrace.ClientTrace {
 		GetConn: func(hostPort string) {},
 		GotConn: func(connInfo httptrace.GotConnInfo) {
-			// Get the local address and port, as "<ip4>:<port>"
+			// Get the local address and port, as "100.100.100.100:1234" or "[a100:a200:a300:a400:a500:a600]:1234"
 			localConnStr := connInfo.Conn.LocalAddr().String()
-			connStrPieces := strings.Split(localConnStr, ":")
-			sourceAddr = connStrPieces[0]
-			sourcePort, err = strconv.Atoi(connStrPieces[1])
+			fmt.Printf("Local address string is %s\n", localConnStr)
+			if strings.Contains(localConnStr, "]:") {
+				fmt.Println("Detected ipv6 local address")
+				connStrPieces := strings.Split(localConnStr, "]:")
+				sourceAddr = connStrPieces[0] + "]"
+				sourcePort, err = strconv.Atoi(connStrPieces[1])
+				check(err)
+			} else {
+				fmt.Println("Assuming ipv4 local address")
+				connStrPieces := strings.Split(localConnStr, ":")
+				sourceAddr = connStrPieces[0]
+				sourcePort, err = strconv.Atoi(connStrPieces[1])
+				check(err)
+			}
+
 			if err != nil {
 				// TODO: What do we do if the port isn't present?
-				fmt.Printf("Local host is addr %s port unknown", sourceAddr)
+				fmt.Printf("Local host is addr %s port unknown\n", sourceAddr)
 			} else {
-				fmt.Printf("Local host is addr %s port %d", sourceAddr, sourcePort)
+				fmt.Printf("Local host is addr %s port %d\n", sourceAddr, sourcePort)
 			}
 		},
 		ConnectStart: func(network string, addr string) {},
@@ -370,7 +540,7 @@ func sendHttpMessage(method string, path string, headers any, body string) (*Mes
 	// Wrap the request with the tracer
 	req = req.WithContext(httptrace.WithClientTrace(req.Context(), trace))
 
-	// Emit the request
+	// Emit the HTTP request
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		return makeErrorResponse("error", path), err
@@ -386,8 +556,11 @@ func sendHttpMessage(method string, path string, headers any, body string) (*Mes
 		responseBodyStr = string(responseBody)
 	}
 
+	// Print the response body and HTTP error code to the console, but do not add to activity log!
+	fmt.Printf("Received HTTP(s) response code %d, and response body:\n=== START ===\n%s\n=== END ===\n\n", resp.StatusCode, responseBodyStr)
+
 	// Return a success
-	return makeSuccessResponse("sent", responseBodyStr, sourceAddr, sourcePort, int(req.ContentLength), path, resp.StatusCode), nil
+	return makeSuccessResponse("sent", sourceAddr, sourcePort, int(req.ContentLength), path), nil
 }
 
 // TODO: Incorporate headers before sending a request?
@@ -398,12 +571,17 @@ func addHeadersAsNeeded(req *http.Request, headers any) {
 func injectPortIntoAddress(addr string, port int, protocol string) (string, error) {
 	switch protocol {
 	case "http", "https":
-		u, err := url.Parse(addr)
+		u, err := url.Parse(protocol + "://" + addr)
 		if err != nil {
 			return "", fmt.Errorf("unable to parse address %s", addr)
 		}
 
-		return strings.Replace(addr, u.Host, u.Host+":"+strconv.Itoa(port), 1), nil
+		hostWithPort := u.Hostname() + ":" + strconv.Itoa(port)
+		fmt.Printf("Replacing '%s' with '%s' in '%s'...\n", u.Hostname(), hostWithPort, addr)
+		newAddress := strings.Replace(addr, u.Hostname(), hostWithPort, 1)
+
+		fmt.Printf("New URL: %s\n", newAddress)
+		return newAddress, nil
 	default:
 		return "", fmt.Errorf("unknown protocol: %s", protocol)
 	}
@@ -416,34 +594,24 @@ func check(e error) {
 }
 
 // TODO: Make this less brittle somehow?
-func isHeader(line string) bool {
+func isCSVHeaderStr(line string) bool {
 	return line == HeaderStr
 }
 
-// Writes the activity log entry to the file.
-func writeActivityLog(logFile *os.File, logInfo *ActivityLogEntry) {
-	// Create a writer and attach it to the log file
-	writer := csv.NewWriter(logFile)
-	defer writer.Flush()
-
-	// Convert the log entry to a row of CSV-formatted strings
-	row := []string{}
-	value := reflect.ValueOf(logInfo)
-	for i := 0; i < value.NumField(); i++ {
-		field := value.Field(i)
-		row = append(row, fmt.Sprintf("%v", field.Interface()))
-	}
-
-	// Write it to the activity log
-	err := writer.Write(row)
-	if err != nil {
-		check(fmt.Errorf("unable to write csv to file"))
-	}
-}
-
-// https://stackoverflow.com/a/31196754
+// https://gist.github.com/lee8oi/ec404fa99ea0f6efd9d1
 func startProcess(cmd string, args []string) (*os.Process, error) {
-	procAttr := new(os.ProcAttr)
+	realCmd, err := exec.LookPath(cmd)
+	if err != nil {
+		return nil, err
+	}
+
+	var procAttr os.ProcAttr
 	procAttr.Files = []*os.File{os.Stdin, os.Stdout, os.Stderr}
-	return os.StartProcess(cmd, args, procAttr)
+	fmt.Printf("Starting command %s with args %v\n", realCmd, args)
+	p, err := os.StartProcess(realCmd, args, &procAttr)
+	if err != nil {
+		return nil, err
+	}
+
+	return p, nil
 }
